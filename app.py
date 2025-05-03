182d75aa2dd210b123c2f896e3eb624625398245
AIfindit <InfoAIfindit@gmail.com>
2025-05-03 17:11:23 +0100

    app.py

--- app.py
+++ app.py
@@ -38,8 +38,8 @@ pipe = StableDiffusionXLInstantIDPipeline.from_pretrained(
 
 pipe.scheduler = DDIMScheduler.from_config(pipe.scheduler.config)
 pipe.load_ip_adapter(
-    "checkpoints/ip-adapter",  # Caminho base
-    subfolder="",              # Se estiver diretamente dentro da pasta
+    pretrained_model_name_or_path_or_dict=None,
+    subfolder="checkpoints/ip-adapter",  # <- pasta onde está o .bin
     weight_name="ip-adapter-plus-face_sd15.bin"
 )
 pipe.set_ip_adapter_scale(0.6)
